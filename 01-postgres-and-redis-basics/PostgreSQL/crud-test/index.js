e8be7850d05c5ff2656dae52c9a6f5b880659b2f
Kartik Gupta <thisiskartikgupta@gmail.com>
2021-08-29 21:18:27 +0530

    Updated .gitignore for node_modules

---
+++ 01-postgres-and-redis-basics/PostgreSQL/crud-test/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (request, response) => {
+//     response.json({ info: 'Node.js, Express, and Postgres API' })
+//   });
+
+// app.listen(port, () => {
+//     console.log(`App running on port ${port}.`)
+//   })
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'test',
+  password: '14844841',
+  port: 5432,
+});
+
+// server": "localhost",
+//             "port": 5432,
+//             "driver": "PostgreSQL",
+//             "name": "postgres-dev0",
+//             "database": "test",
+//             "username": "postgres",
+//             "password": "14844841"
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM test ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      console.log(json(results.rows))
+    })
+  }
