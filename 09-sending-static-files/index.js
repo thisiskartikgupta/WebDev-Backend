d3a9c5afb93b4cdc33d395ae8b1522d2c4dde0fb
Kartik Gupta <thisiskartikgupta@gmail.com>
2021-11-29 22:32:39 +0530

    Added Day 9

---
+++ 09-sending-static-files/index.js
@@ -0,0 +1,6 @@
+// 1. Send and download static files using sendFile and make a static folder for the same.
+//    Use path and __dirname. Filetypes like jpg png svg html txt pdf
+// 2. View engines - Jade. Jade template engine . Make a views folder for the same.
+//    Read about res.render for sending static content. Other engines includes EJS. app.set()
+// 3. Setting cookies using res pipelining. res.status().cookie().cookie() with expire time setting
+// 4. Redirecting a user using res.redirect() . Also use this with pipelining.
